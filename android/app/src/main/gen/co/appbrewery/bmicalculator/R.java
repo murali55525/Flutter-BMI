a5ae6e85fd7d47cedb55388d41173d482bc04d52
murali55525 <m.muralikarthick123@gmail.com>
2024-06-17 16:50:00 +0530

    ad

--- android/app/src/main/gen/co/appbrewery/bmicalculator/R.java
+++
@@ -1,5 +0,0 @@
-package co.appbrewery.bmicalculator;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'screens/input_page.dart';
+import 'MainScreen.dart';
 
-void main() => runApp(BMICalculator());
-
-class BMICalculator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
-      ),
-      home: InputPage(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: InputPage(),
+  ));
 }
